b37146cfff7ff5ceba7e18a675cde26bb79e56fc
KarimNaja <karimnaja3@gmail.com>
2026-01-07 23:51:51 +0300

    Initial Commit

---
+++ Apple-project/src/App.jsx
@@ -0,0 +1,15 @@
+import Navbar from "./components/Navbar.jsx"
+import Hero from "./components/Hero.jsx"
+import ProductViewer from "./components/ProductViewer.jsx"
+
+const App = () => {
+  return (
+    <main>
+        <Navbar />
+        <Hero />
+        <ProductViewer />
+    </main>
+  )
+}
+
+export default App
